3f6187f65bbaa2342d5738ed19fa7775156f0717
Vinare <scrat-me@mail.ru>
2021-12-02 21:11:13 +0300

    Lesson 4 done

--- complicated.js
+++ complicated.js
@@ -1,45 +1,30 @@
 'use strict'
 
-//***1.1***//
 
-let lang_array = []
-
-let lang = 'ru'
-
-lang = 'en'
-
-if (lang == 'ru') {
-  lang_array = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс']
-} else if (lang == 'en') {
-  lang_array = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-}
-
-console.log(lang_array)
-
-//1.2/
-
-switch(lang) {
-  case 'ru': 
-    console.log(lang_array = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс'])
-    break
-  case 'en':
-    console.log(lang_array = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'])
-    break
-}
-
-//1.3/
-lang_array['ru'] = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс']
-lang_array['en'] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-
-console.log(lang_array[lang]);
-
-// ***2***//
+// Из 3-его урока//
 
 let namePerson = 'Артем'
 
 namePerson = 'Александр'
 
 namePerson == 'Артем' ? console.log('Директор') : namePerson == 'Александр' ? console.log('Преподаватель') : console.log('Студент')
+
+//  Урок 4  //
+
+let str = 'string'
+
+function notaString(str) {
+  if (typeof str !== 'string') {
+    alert('Аргумент не строка!')
+  } else {
+    console.log(typeof str)
+  }
+
+  str = str.trim() 
+  return str.length > 30 ? str.slice(0, 30) + '...' : str;
+}
+
+notaString(str)
 
 
 
