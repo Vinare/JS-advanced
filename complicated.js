bbe92305ecfc7ab1277990be5f1f069cdbe7716b
Vinare <scrat-me@mail.ru>
2021-12-07 21:59:47 +0300

    1-st complicated

--- complicated.js
+++ complicated.js
@@ -1,31 +1,12 @@
 'use strict'
 
+let notNumber
 
-// Из 3-его урока//
+ while (isNaN(notNumber) || notNumber.toString().trim() === "" || notNumber === null) {
+   notNumber = +prompt('Bведите число')
+ }
 
-let namePerson = 'Артем'
-
-namePerson = 'Александр'
-
-namePerson == 'Артем' ? console.log('Директор') : namePerson == 'Александр' ? console.log('Преподаватель') : console.log('Студент')
-
-//  Урок 4  //
-
-let str = 'string'
-
-function notaString(str) {
-  if (typeof str !== 'string') {
-    alert('Аргумент не строка!')
-  } else {
-    console.log(typeof str)
-  }
-
-  str = str.trim() 
-  return str.length > 30 ? str.slice(0, 30) + '...' : str;
-}
-
-notaString(str)
-
+ console.log (typeof notNumber)
 
 
 
