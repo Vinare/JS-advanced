701336e2ca742a50a63ccb266c3c331200d9ad7c
Vinare <scrat-me@mail.ru>
2021-12-08 15:59:10 +0300

    complicated-5 done

--- complicated.js
+++ complicated.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//  №1 //
+
 let notNumber
 
 notNumber = +prompt('Bведите число')
@@ -10,5 +12,23 @@ notNumber = +prompt('Bведите число')
 
  console.log(notNumber, typeof notNumber)
 
+//  №2  //
 
+let arr = ['22', '38', '486', '298', '569', '46', '122']
 
+arr.forEach((number) => {
+  if (number.startsWith('2') || number.startsWith('4')) {
+  console.log(number)
+  }
+})
+
+//  №3  //
+
+function isPrime(n) {
+  for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
+  return n > 1;
+}
+
+const res = [...Array(101)].reduce((a, _, i) => a.concat(isPrime(i) ? `${i}: Делители этого числа: 1 и ${i}` : []) , []).join('\n')
+
+console.log(res)
