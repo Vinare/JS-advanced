f63dcc0e97c9f69180f5bdbb1afe34840deffa41
Vinare <scrat-me@mail.ru>
2021-12-21 23:11:37 +0300

    сволочь

--- complicated.js
+++ complicated.js
@@ -1,23 +1,125 @@
-'use strict';
+"use strict";
 
-const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-const day = document.getElementById('fordays');  // Получаем div куда будем добавлять дни недели
-const options = { weekday: 'long'};
-const todayDay = new Date().toLocaleDateString('ru-RU', options);
-console.log(todayDay);
+const appData = {
+  title: '', 
+  screens: [], 
+  screenPrice: 0, 
+  adaptive: true,  
+  rollback: 5,
+  allServicePrices: 0,
+  fullPrice: 0, 
+  servicePercentPrice: 0, 
+  services: {}, 
 
-week.forEach((item, i) => {
-  let newdiv = document.createElement('div');   // Для каждого элемента массива week создаём свой div
-  if (item.toLowerCase() === todayDay) {    // Если текущий день недели соответствует дню недели массива
-    newdiv.classList.add('today');    //Вешаем на него класс bold 
-    newdiv.innerHTML = week[i];    // Вставляем текст в div
+  start: function() {
+    appData.asking();
+    appData.addPrices();
+    appData.getFullPrice();
+    appData.getServicePercentPrice();
+    appData.getTitle(); 
+    appData.logger();
+  },
+
+  asking: function() {
+    appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+
+    while (!appData.isString(appData.title)) {
+        appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
+    }
+
+    for (let i = 0; i < 2; i++) {
+      let name = prompt('Какие типы экранов нужно разработать?');
+
+      while (!appData.isString(name)) {
+          name = prompt('Какие типы экранов нужно разработать?');
+      }
+
+      let price = 0;
+
+      do {
+          price = prompt('Сколько будет стоить данная работа?');
+      } while (!appData.isNumber(price));
+
+      appData.screens.push({id: i, name: name, price: price});  // формируем массив объектов с ключом и значением
+    }
+    
+    for (let i = 0; i < 2; i++) {
+      let name = prompt('Какой дополнительный тип услуги нужен?');
+      
+      while (!appData.isString(name)) {
+          name = prompt('Какой дополнительный тип услуги нужен?');
+      }
+      
+      let price = 0;
+
+      // if (i === 0) {
+      //   appData.name1 = prompt('Какой дополнительный тип услуги нужен?');
+      // } else if (i === 1) {
+      //   appData.name2 = prompt('Какой дополнительный тип услуги нужен?');
+      // } 
+
+      do {
+          price = prompt('Сколько это будет стоить?');
+      } while (!appData.isNumber(price)); 
+
+      appData.services[name] = +price; // собираем в объект services {} все ответы на вопросы поль-лю
+    }
+
+    appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+  },
+
+  isString: function(string) {
+    return isNaN(string);
+  }, 
+  
+  isNumber: function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  },
+
+  addPrices: function() {  // суммируем общую стоимость экранов и услуг
+    for (let screen of appData.screens) {
+        appData.screenPrice += +screen.price;  // суммируем стоимость всех экранов
+    }
+
+    for(let key in appData.services) {         // суммируем стоимость всех доп услуг
+      appData.allServicePrices += appData.services[key];
+    }
+  },
+  
+  getFullPrice: function() {
+    appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
+  },
+  
+  getServicePercentPrice: function() {
+    appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
+  },
+  
+  getTitle: function() {
+    appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().slice(1).toLowerCase();
+  },
+  
+  getRollbackMessage: function(price) {
+    if (price >= 30000) {
+      return 'Даем скидку в 10%';
+    } else if (price < 30000 && price >= 15000) {
+      return 'Даем скидку в 5%';
+    } else if (price < 15000 && price > 0) {
+      return 'Скидка не предусмотрена';
+    } else {
+      return 'Что-то пошло не так';
+    }
+  },
+  logger: function() {
+    console.log(appData.fullPrice);
+    console.log(appData.servicePercentPrice);
+    console.log(appData.services);
+    console.log(appData.screens);
   }
-  if (item === "Суббота" || item === "Воскресенье") {    //Если выходные то
-    newdiv.classList.add('italic');    // Выделяем день недели дополнительно курсивом 
-    newdiv.innerHTML = week[i];    // Вставляем текст в div
-  } else {       // Если не текущий день и не выходные,
-    newdiv.innerHTML = week[i];    // то просто вставляем текст без стилей
-  }
-  day.appendChild(newdiv);  // к предварительно созданному родительскому div добавляем доерние newdiv' ы 
-});
+};
+
+appData.start();
+
+
+
+
 
