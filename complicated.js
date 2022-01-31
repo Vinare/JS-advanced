c40631d3d3a5d6508532952c9ec9f67f549e31d4
Vinare <scrat-me@mail.ru>
2022-01-31 15:18:57 +0300

    Code refactoring

--- complicated.js
+++ complicated.js
@@ -1,45 +1,46 @@
-'use strict'
+'use strict';
 
 //***1.1***//
 
-let lang_array = []
+let langArray = [];
 
-let lang = 'ru'
+let lang = 'ru';
+lang = 'en';
 
-lang = 'en'
-
-if (lang == 'ru') {
-  lang_array = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс']
-} else if (lang == 'en') {
-  lang_array = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+if (lang === 'ru') {
+  langArray = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+} else if (lang === 'en') {
+  langArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 }
 
-console.log(lang_array)
+console.log(langArray);
 
-//1.2/
+//1.2//
 
 switch(lang) {
   case 'ru': 
-    console.log(lang_array = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс'])
-    break
+    langArray = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+  break;
   case 'en':
-    console.log(lang_array = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'])
-    break
+    langArray = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  break;
 }
 
-//1.3/
-lang_array['ru'] = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс']
-lang_array['en'] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+console.log(langArray);
 
-console.log(lang_array[lang]);
+//1.3//
+langArray['ru'] = ['Пн', 'Вт', 'СР', 'Чт', 'Пт', 'Сб', 'Вс'];
+langArray['en'] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+console.log(langArray[lang]);
 
 // ***2***//
 
-let namePerson = 'Артем'
+let namePerson = 'Артем';
 
-namePerson = 'Александр'
+namePerson = 'Александр';
 
-namePerson == 'Артем' ? console.log('Директор') : namePerson == 'Александр' ? console.log('Преподаватель') : console.log('Студент')
+namePerson = namePerson == 'Артем' ? console.log('Директор') : namePerson == 'Александр' ? console.log('Преподаватель') : console.log('Студент');
 
 
 
