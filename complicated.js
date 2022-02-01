5fafbaf9cee2ad2e58f606d324babe7a9da43371
Vinare <scrat-me@mail.ru>
2022-02-01 18:50:44 +0300

    Code refactoring

--- complicated.js
+++ complicated.js
@@ -1,30 +1,19 @@
-'use strict'
+'use strict';
 
-
-// Из 3-его урока//
-
-let namePerson = 'Артем'
-
-namePerson = 'Александр'
-
-namePerson == 'Артем' ? console.log('Директор') : namePerson == 'Александр' ? console.log('Преподаватель') : console.log('Студент')
-
-//  Урок 4  //
-
-let str = 'string'
+let str = 'string';
 
 function notaString(str) {
   if (typeof str !== 'string') {
-    alert('Аргумент не строка!')
+    alert('Аргумент не строка!');
   } else {
-    console.log(typeof str)
+    console.log(typeof str);
   }
 
-  str = str.trim() 
+  str = str.trim(); 
   return str.length > 30 ? str.slice(0, 30) + '...' : str;
 }
 
-notaString(str)
+notaString(str);
 
 
 
