266b348f52eda00357cbc17d5cf258c06ba38efb
Vinare <scrat-me@mail.ru>
2022-02-09 01:43:22 +0300

    Done

--- complicated.js
+++ complicated.js
@@ -1,118 +1,47 @@
 "use strict";
 
-const appData = {
-  title: '', 
-  screens: [], 
-  screenPrice: 0, 
-  adaptive: true,  
-  rollback: 5,
-  allServicePrices: 0,
-  fullPrice: 0, 
-  servicePercentPrice: 0, 
-  services: {}, 
+// 1.a) //
 
-  start: function() {
-    appData.asking();
-    appData.addPrices();
-    appData.getFullPrice();
-    appData.getServicePercentPrice();
-    appData.getTitle(); 
-    appData.logger();
-  },
+let options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  };
 
-  asking: function() {
-    appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
-
-    while (!appData.isString(appData.title)) {
-        appData.title = prompt('Как называется ваш проект?', 'Калькулятор верстки');
-    }
-
-    for (let i = 0; i < 2; i++) {
-      let name = prompt('Какие типы экранов нужно разработать?');
-
-      while (!appData.isString(name)) {
-          name = prompt('Какие типы экранов нужно разработать?');
-      }
-
-      let price = 0;
-
-      do {
-          price = prompt('Сколько будет стоить данная работа?');
-      } while (!appData.isNumber(price));
-
-      appData.screens.push({id: i, name: name, price: +price});  // формируем массив объектов с ключом и значением
-    }
-    
-    for (let i = 0; i < 2; i++) {
-      let name = prompt('Какой дополнительный тип услуги нужен?');
-      
-      while (!appData.isString(name)) {
-          name = prompt('Какой дополнительный тип услуги нужен?');
-      }
-      
-      let price = 0;
-
-      do {
-          price = prompt('Сколько это будет стоить?');
-      } while (!appData.isNumber(price));  
-      
-      appData.services[name + (i + 1)] = +price;  // собираем в объект services {} все ответы на вопросы о доп услугах
-    }
-
-    appData.adaptive = confirm('Нужен ли адаптив на сайте?');
-  },
-
-  isString: function(string) {
-    return isNaN(string);
-  }, 
+let todayDate = new Date().toLocaleString('ru', options);
+let hours = new Date().getHours();
+let min = new Date().getMinutes();
+let sec = new Date().getSeconds();
+let correctTime;
   
-  isNumber: function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-  },
+function declOfNum(n, textForms) {   
+  n = Math.abs(n) % 100; 
+  var n1 = n % 10;
+  if (n > 10 && n < 20) { return textForms[2]; }
+  if (n1 > 1 && n1 < 5) { return textForms[1]; }
+  if (n1 == 1) { return textForms[0]; }
+  return textForms[2];
+}
 
-  addPrices: function() {  // суммируем общую стоимость экранов и услуг
-   
-    appData.screenPrice = appData.screens.reduce(function(sum, screen) {  
-      return sum + screen.price;           // суммируем стоимость всех экранов методом reduce
-    }, 0);   
-    
-    for(const service in appData.services) {  // суммируем стоимость всех доп услуг
-      appData.allServicePrices += appData.services[service];
-    }
-  },
-  
-  getFullPrice: function() {
-    appData.fullPrice = +appData.screenPrice + appData.allServicePrices;
-  },
-  
-  getServicePercentPrice: function() {
-    appData.servicePercentPrice = appData.fullPrice - (appData.fullPrice * (appData.rollback / 100));
-  },
-  
-  getTitle: function() {
-    appData.title = appData.title.trim()[0].toUpperCase() + appData.title.trim().slice(1).toLowerCase();
-  },
-  
-  getRollbackMessage: function(price) {
-    if (price >= 30000) {
-      return 'Даем скидку в 10%';
-    } else if (price < 30000 && price >= 15000) {
-      return 'Даем скидку в 5%';
-    } else if (price < 15000 && price > 0) {
-      return 'Скидка не предусмотрена';
-    } else {
-      return 'Что-то пошло не так';
-    }
-  },
-  logger: function() {
-    console.log(appData.fullPrice);
-    console.log(appData.servicePercentPrice);
-    console.log(appData.services);
-    console.log(appData.screens);
-  }
-};
+correctTime = `${hours} ${declOfNum(hours, ['час', 'часа', 'часов'])} ${min} ${declOfNum(min, ['минута', 'минуты', 'минут'])} ${sec} ${declOfNum(sec, ['секунда', 'секунды', 'секунд'])}`;
 
-appData.start();
+function getCurrentTime() {
+  console.log('Сегодня ' + todayDate[0].toUpperCase() + todayDate.slice(1).replace('г.', 'года, ') + correctTime);
+}
+
+setInterval(getCurrentTime, 1000);
+
+// 1.b //
+
+let today = new Date().toLocaleString('ru').replace(',', ' -' );
+
+console.log(today);
+
+
+
+
+
 
 
 
